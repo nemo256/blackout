de270e05cb516890bb9540db8f019e61179438d1
Amine Neggazi <neggazimedlamine@gmail.com>
2023-01-12 14:49:35 +0100

    removed unnecessary files

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,22 +4,38 @@ import { useState, useRef, useEffect } from "react";
 import styles from '../styles/Home.module.css'
 
 const banner = `
-Initializing AnderShell 3000 v0.1
-Copyright (c) 2014 Anders Evenrud <andersevenrud@gmail.com>
-
-Type 'help for a list of available commands.
-
-
+        | Welcome to blackout |
+          ⠀⠀⠀⠀⠀⠀⠀⠀⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+          ⠀⢀⣄⠀⣠⠶⠲⠞⠁⠀⠙⠛⠳⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+          ⠀⡟⠙⠛⠁⣀⣀⢀⡤⢤⠀⠀⠀⠙⢷⣄⠀⠀⠀⠀⠀⠀⠀
+          ⢠⡷⢄⣠⠊⠀⠀⠁⠀⡀⠑⠒⠈⢳⠀⢻⡆⠀⠀⠀⠀⠀⠀
+          ⠀⣷⠃⢠⡀⠀⠀⠀⠀⠈⠀⠀⠀⢎⠀⢸⡇⠀⠀⠀⠀⠀⠀
+          ⢠⡇⠀⠘⢁⡄⠀⠀⠉⠉⠀⠀⠀⣳⢧⣾⠃⠀⠀⠀⠀⠀⠀
+          ⢸⡇⠀⠀⠘⠆⠀⠀⢀⠀⠀⠀⠀⠁⢿⡏⠀⠀⠀⠀⠀⠀⠀
+          ⠈⣇⠸⢖⡀⠀⠐⣂⠹⡇⠀⠀⠀⣀⣼⠇⠀⠀⠀⠀⠀⠀⠀
+          ⠀⠹⣦⠀⠈⠭⠉⠀⠀⠀⠀⣠⡾⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀
+          ⠀⠀⠈⠳⢦⣄⣀⣀⣠⡴⠞⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+          ⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 `;
+
+const help = `
+    Available commands:
+
+start - Start the game
+help - Display this output
+clear - Clears the display
+`
 
 export default function Home() {
   const [input, setInput] = useState("$ > ")
   const [output, setOutput] = useState("")
   const inputRef = useRef()
 
+
   useEffect(() => {
     inputRef.current.focus()
-  })
+    setOutput(banner)
+  }, [])
 
   return (
     <>
@@ -29,7 +45,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main} onClick={e => {inputRef.current.focus()}}>
+      <main className={styles.main} onClick={e => {
+        setInput("$ > ")
+        inputRef.current.focus()
+      }}>
         <input 
           ref={inputRef} 
           type="text"
@@ -42,11 +61,17 @@ export default function Home() {
               let newOutput = " "
               newOutput = output + "\n"
               switch (input) {
+                case "$ > start":
+                  newOutput = ""
+                  break;
+                case "$ > help":
+                  newOutput = help
+                  break;
                 case "$ > clear":
                   newOutput = ""
                   break;
                 default:
-                  newOutput += "Command not found!"
+                  newOutput += "Command not found (try help)!"
                   break;
               }
               setOutput(newOutput)
